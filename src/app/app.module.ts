c83bd7b91951482999d6beba23a2292b638f171a
Alexander van den Bogert <vdbalexander2208@gmail.com>
2020-01-27 11:53:16 +0100

    bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 const​ routes​:​ ​Routes​ ​=​ [{ path: ​''​, redirectTo: ​'home'​, pathMatch: ​'full'​},   { path: ​'home'​, component: HomeComponent },   { path: ​'favie'​, component: FavieComponent},   { path: ​'contact'​, component: ContactComponent}   ]; 
 
+
+
 @NgModule({
   declarations: [
     AppComponent,
